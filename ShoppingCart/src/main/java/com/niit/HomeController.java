30dc9194abbe71d409a2e392a4862ab7fd684984
Aswini Suresh <mailtoaswini28@gmail.com>
2017-03-14 07:58:10 -0400

    ezone

---
+++ ShoppingCart/src/main/java/com/niit/HomeController.java
@@ -0,0 +1,10 @@
+package com.niit;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+	public HomeController()
+	{
+		System.out.println("Controller");
+	}
+}
